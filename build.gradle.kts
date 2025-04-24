ea669a8f68677773c4404670d22ec2cc90660edc
Łukasz Budnik <lukasz.budnik@gmail.com>
2025-04-24 21:14:08 +0200

    Switching to Gradle catalog for managing dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,29 +20,30 @@ repositories {
 val mockitoAgent = configurations.create("mockitoAgent")
 
 dependencies {
-    implementation("org.rocksdb:rocksdbjni:10.0.1")
-    implementation("com.esotericsoftware:kryo:5.6.2")
-    implementation("commons-io:commons-io:2.19.0")
+    implementation(libs.rocksdb)
+    implementation(libs.kryo)
+    implementation(libs.commons.io)
 
-    implementation("com.google.protobuf:protobuf-java:4.30.2")
-    implementation("com.google.protobuf:protobuf-java-util:4.30.2")
-    implementation("io.grpc:grpc-protobuf:1.71.0")
-    implementation("io.grpc:grpc-services:1.72.0")
-    implementation("io.grpc:grpc-stub:1.72.0")
-    implementation("io.grpc:grpc-netty-shaded:1.71.0")
-    implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation(libs.protobuf.java)
+    implementation(libs.protobuf.java.util)
+    implementation(libs.grpc.protobuf)
+    implementation(libs.grpc.services)
+    implementation(libs.grpc.stub)
+    implementation(libs.grpc.netty.shaded)
+    implementation(libs.javax.annotation)
 
-    implementation("org.slf4j:slf4j-api:2.0.17")
-    implementation("ch.qos.logback:logback-classic:1.5.18")
+    implementation(libs.slf4j.api)
+    implementation(libs.logback.classic)
 
-    testImplementation(platform("org.junit:junit-bom:5.12.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("io.grpc:grpc-inprocess:1.72.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.17.0")
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly(libs.junit.platform.launcher)
+    testImplementation(libs.grpc.inprocess)
+    testImplementation(libs.mockito.junit)
 
-    mockitoAgent("org.mockito:mockito-core:5.17.0") { isTransitive = false }
+    mockitoAgent(libs.mockito.core) { isTransitive = false }
 }
+
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
